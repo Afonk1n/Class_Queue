71a447b6a9c690c435b80180907bd1d97a34b154
Kirito <afona2001@mail.ru>
2023-11-19 13:55:10 +0500

    add user interface

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection.Emit;
+using static System.Collections.Specialized.BitVector32;
 
 namespace Class_Queue
 {
@@ -11,7 +8,79 @@ namespace Class_Queue
     {
         static void Main(string[] args)
         {
-            Queue<int> myQueue = new Queue<int>(5);
+            Console.WriteLine("Enter queue length");
+
+            try
+            {
+                int length = Convert.ToInt32(Console.ReadLine());
+                Queue<int> testQueue = new Queue<int>(length);
+                int action = 0;
+                while (action != 8) {
+                    Console.WriteLine("Choose an action:\n1.Add item to queue Enqueue\n2.Remove an element from the beginning of the queue Dequeue\n3.Return an element from the beginning of the queue Peek\n4.Check if an element is in the queue Contains\n5.Convert queue to array ToArray\n6.Convert queue to string ToString\n7.Clears the queue\n8.Exit");
+                    action = Convert.ToInt32(Console.ReadLine());
+                    switch (action)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Enter element to be added to the queue");
+                                int element = Convert.ToInt32(Console.ReadLine());
+                                testQueue.Enqueue(element);
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("Removed element: " + testQueue.Dequeue());
+                                break;
+                            }
+                        case 3:
+                            {
+                                Console.WriteLine("First element in queue: " + testQueue.Peek());
+                                break;
+                            }
+                        case 4:
+                            {
+                                Console.WriteLine("Enter an item to check if it is in the queue");
+                                int element = Convert.ToInt32(Console.ReadLine());
+                                bool containsElement = testQueue.Contains(element);
+                                Console.WriteLine("Does the element contain: " + containsElement);
+                                break;
+                            }
+                        case 5:
+                            {
+                                int[] toArray = testQueue.ToArray();
+                                string str = string.Join(", ", toArray);
+                                Console.WriteLine("Array: " + str);
+                                break;
+                            }
+                        case 6:
+                            {
+                                string str = testQueue.ToString();
+                                Console.WriteLine("String: " + str);
+                                break;
+                            }
+                        case 7:
+                            {
+                                testQueue.Clear();
+                                Console.WriteLine("Queue cleared");
+                                break;
+                            }
+                        case 8:
+                            {
+                                break;
+                            }
+                        default:
+                            {
+                                break;
+                            }
+                    }
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                
+            }
+
+            /*Queue<int> myQueue = new Queue<int>(5);
             
             myQueue.Enqueue(1);
             myQueue.Enqueue(2);
@@ -40,7 +109,7 @@ namespace Class_Queue
             while (!myQueue.IsEmpty)
             {
                 Console.WriteLine("Извлечен элемент: " + myQueue.Dequeue());
-            }
+            }*/
             Console.ReadLine();
         }
     }
